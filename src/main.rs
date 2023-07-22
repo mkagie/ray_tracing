b3c7d5b07ecd3e18475e6159c0f3be69dfd4a464
Matt Kagie <mjkagie@gmail.com>
2023-07-22 11:35:08 -0700

    Finished Chp10

--- src/main.rs
+++ src/main.rs
@@ -272,6 +272,63 @@ impl Scatterable for Metal {
     }
 }
 
+/// A Dielectric is a refractive material, such as glass
+#[derive(Debug, Clone)]
+pub struct Dielectric {
+    ir: f64,
+}
+impl Dielectric {
+    pub fn new(ir: f64) -> Self {
+        Self { ir }
+    }
+
+    fn refract(uv: &Vec3, n: &Vec3, etai_over_etat: f64) -> Vec3 {
+        let cos_theta = -uv.dot(&n).min(1.0);
+        let r_out_perp = etai_over_etat * (uv + cos_theta * n);
+        let r_out_parallel = -(1.0 - r_out_perp.norm().powi(2)).abs().sqrt() * n;
+        r_out_perp + r_out_parallel
+    }
+
+    fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+        // Use Schlick's approximation for reflectance
+        let r0 = ((1.0 - ref_idx) / (1.0 + ref_idx)).powi(2);
+        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+    }
+}
+impl Scatterable for Dielectric {
+    fn try_scatter(&self, ray_in: &Ray, hit_record: &HitRecord) -> Option<ScatterResult> {
+        let attenuation = Color::new(1.0, 1.0, 1.0);
+        let refraction_ratio = if hit_record.front_face {
+            1.0 / self.ir
+        } else {
+            self.ir
+        };
+
+        let unit_direction = ray_in.dir.normalize();
+        let cos_theta = -unit_direction.dot(&hit_record.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
+
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let mut rng = rand::thread_rng();
+
+        let direction = if cannot_refract
+            || Self::reflectance(cos_theta, refraction_ratio) > rng.gen::<f64>()
+        {
+            // TODO(mkagie) Remove reflect and refract from Metal and Dielectric, since they are no
+            // longer tied to them
+            Metal::reflect(&unit_direction, &hit_record.normal)
+        } else {
+            Self::refract(&unit_direction, &hit_record.normal, refraction_ratio)
+        };
+
+        let scattered = Ray::new(hit_record.p, direction);
+        Some(ScatterResult {
+            attenuation,
+            scattered,
+        })
+    }
+}
+
 /// Module for random utils
 pub mod utils {
     use nalgebra::Vector3;
@@ -340,9 +397,12 @@ fn main() {
 
     // Define materials
     let material_ground = Box::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Box::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
-    let material_left = Box::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
-    let material_right = Box::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
+    // let material_center = Box::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
+    let material_center = Box::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    // let material_center = Box::new(Dielectric::new(1.5));
+    // let material_left = Box::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
+    let material_left = Box::new(Dielectric::new(1.5));
+    let material_right = Box::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
 
     world.add(Box::new(Sphere::new(
         Point::new(0.0, -100.5, -1.0),
@@ -357,6 +417,11 @@ fn main() {
     world.add(Box::new(Sphere::new(
         Point::new(-1.0, 0.0, -1.0),
         0.5,
+        material_left.clone(),
+    )));
+    world.add(Box::new(Sphere::new(
+        Point::new(-1.0, 0.0, -1.0),
+        -0.4,
         material_left,
     )));
     world.add(Box::new(Sphere::new(
