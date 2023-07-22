b9b854c89e83df8b62a236377382560a6634eaef
Matt Kagie <mjkagie@gmail.com>
2023-07-21 21:30:11 -0700

    Completed chp 7

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 //! Code to generate a ray tracer, working through the examples
 use indicatif::ProgressBar;
 use nalgebra::Vector3;
+use rand::Rng;
+use rayon::prelude::*;
 
 type Vec3 = Vector3<f64>;
 type Point = Vec3;
 type Color = Vec3;
 
-fn write_color(color: &Color) {
-    let ir = (255.999 * color[0]).round() as usize;
-    let ig = (255.999 * color[1]).round() as usize;
-    let ib = (255.999 * color[2]).round() as usize;
-    println!("{ir} {ig} {ib}");
+fn write_color(color: &Color, samples_per_pixel: usize) {
+    let scale = 1.0 / samples_per_pixel as f64;
+    let r = (256.0 * (color[0] * scale).min(0.999).max(0.0)) as u64;
+    let g = (256.0 * (color[1] * scale).min(0.999).max(0.0)) as u64;
+    let b = (256.0 * (color[2] * scale).min(0.999).max(0.0)) as u64;
+    println!("{r} {g} {b}");
 }
 
 #[derive(Debug)]
@@ -19,11 +22,8 @@ pub struct Ray {
     pub dir: Vec3,
 }
 impl Ray {
-    pub fn new(orig: &Point, dir: &Vec3) -> Self {
-        Self {
-            orig: orig.clone(),
-            dir: dir.clone(),
-        }
+    pub fn new(orig: Point, dir: Vec3) -> Self {
+        Self { orig, dir }
     }
 
     pub fn get(&self, t: f64) -> Point {
@@ -156,11 +156,49 @@ pub trait Hittable {
     fn try_hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
 }
 
+/// Camera and related tasks
+#[derive(Debug)]
+pub struct Camera {
+    origin: Point,
+    lower_left_corner: Point,
+    horizontal: Vec3,
+    vertical: Vec3,
+}
+impl Default for Camera {
+    fn default() -> Self {
+        let aspect_ratio = 16.0 / 9.0;
+        let viewport_height = 2.0;
+        let viewport_width = aspect_ratio * viewport_height;
+        let focal_length = 1.0;
+
+        let origin = Point::zeros();
+        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        let lower_left_corner =
+            origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+        Self {
+            origin,
+            lower_left_corner,
+            horizontal,
+            vertical,
+        }
+    }
+}
+impl Camera {
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+        Ray::new(
+            self.origin.clone(),
+            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+        )
+    }
+}
+
 fn main() {
     // Setup Image
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     let image_width: usize = 400;
     let image_height: usize = (image_width as f64 / ASPECT_RATIO).round() as usize;
+    let samples_per_pixel = 20;
 
     // Setup Camera
     let viewport_height = 2.0;
@@ -179,18 +217,26 @@ fn main() {
     world.add(Box::new(Sphere::new(Point::new(0.0, 0.0, -1.0), 0.5)));
     world.add(Box::new(Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0)));
 
+    // Create the camera
+    let cam = Camera::default();
+
+    // Random sampler
+    let mut rng = rand::thread_rng();
+
     // Render
     print!("P3\n{image_width} {image_height}\n255\n");
-    let bar = ProgressBar::new((image_width * image_height) as u64);
+    let bar = ProgressBar::new((image_width * image_height * samples_per_pixel) as u64);
     for j in (0..=image_height - 1).rev() {
         for i in 0..image_width {
-            let u = i as f64 / (image_width - 1) as f64;
-            let v = j as f64 / (image_height - 1) as f64;
-            let dir = lower_left_corner + u * horizontal + v * vertical - origin;
-            let ray = Ray::new(&origin, &dir);
-            let color = ray.get_color(&world);
-            write_color(&color);
-            bar.inc(1);
+            let mut pixel_color = Color::zeros();
+            for s in 0..samples_per_pixel {
+                let u = (i as f64 + rng.gen::<f64>()) / (image_width - 1) as f64;
+                let v = (j as f64 + rng.gen::<f64>()) / (image_height - 1) as f64;
+                let ray = cam.get_ray(u, v);
+                pixel_color += ray.get_color(&world);
+                bar.inc(1);
+            }
+            write_color(&pixel_color, samples_per_pixel);
         }
     }
     bar.finish()
